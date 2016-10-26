7a34b8325cd1fcb76f9c556b066518306aeb69ca
famoser <git@famoser.ch>
2016-10-26 18:55:02 +0200

    adding new test

--- Famoser.SyncApi.NUnitTests/RepositoryTests.cs
+++ Famoser.SyncApi.NUnitTests/RepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Famoser.SyncApi.Helpers;
 using Famoser.SyncApi.NUnitTests.Implementations;
 using Famoser.SyncApi.NUnitTests.Models;
@@ -10,7 +11,7 @@ namespace Famoser.SyncApi.NUnitTests
     public class RepositoryTests
     {
         [Test]
-        public async Task TestSync()
+        public async Task TestSave()
         {
             //arrange
             var helper = new SyncApiHelper(new StorageService(), "test", "https://test.syncapi.famoser.ch");
@@ -23,6 +24,29 @@ namespace Famoser.SyncApi.NUnitTests
             //assert
             Assert.IsTrue(saveRes);
             Assert.IsTrue(repo.GetAllLazy().Contains(model));
+        }
+
+        [Test]
+        public async Task TestSaveAndRetrieve()
+        {
+            //arrange
+            var ss = new StorageService();
+            var helper = new SyncApiHelper(ss, "test", "https://test.syncapi.famoser.ch");
+            var repo = helper.ResolveRepository<NoteModel>();
+            var model = new NoteModel { Content = "Hallo Welt!" };
+
+            var helper2 = new SyncApiHelper(ss, "test", "https://test.syncapi.famoser.ch");
+            var repo2 = helper2.ResolveRepository<NoteModel>();
+
+            //act
+            var saveRes = await repo.SaveAsync(model);
+            var model2 = await repo2.GetAllAsync();
+
+            //assert
+            Assert.IsTrue(saveRes);
+            Assert.IsTrue(repo.GetAllLazy().Contains(model));
+            Assert.IsTrue(model2.Count == 1);
+            Assert.IsTrue(model2[0].Content == "Hallo Welt");
         }
     }
 }
