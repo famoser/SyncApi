97a3f54560f6be2234677fe9aa79e9e3286d36a9
famoser <git@famoser.ch>
2017-01-26 17:24:21 +0100

    new unit test, still working

--- Famoser.SyncApi.NUnitTests/Repository/SyncRepositoryTests.cs
+++ Famoser.SyncApi.NUnitTests/Repository/SyncRepositoryTests.cs
@@ -11,7 +11,6 @@ namespace Famoser.SyncApi.NUnitTests.Repository
     public class SyncRepositoryTests
     {
         [Test]
-        //[Ignore("Not ready yet")]
         public async Task TestSaveAndRetrieve()
         {
             //arrange
@@ -35,6 +34,41 @@ namespace Famoser.SyncApi.NUnitTests.Repository
             Assert.IsTrue(repo.GetAllLazy().Contains(model));
             Assert.IsTrue(model2.Count == 1);
             Assert.IsTrue(model2[0].Content == "Hallo Welt!");
+        }
+
+        [Test]
+        public async Task TestSaveThreeAndRetrieve()
+        {
+            //arrange
+            var ss = new StorageService();
+            var helper = TestHelper.GetOnlineApiHelper(ss);
+
+            var repo = helper.ResolveRepository<NoteModel>();
+            var model = new NoteModel { Content = "Hallo Welt!" };
+
+            var helper2 = TestHelper.GetOnlineApiHelper(ss);
+            var repo2 = helper2.ResolveRepository<NoteModel>();
+
+            //act
+            await repo.SaveAsync(model);
+            model.Content = "Hallo Welt 2";
+            await repo.SaveAsync(model);
+            model.Content = "Hallo Welt 3";
+            await repo.SaveAsync(model);
+            //clear cache to ensure the notemodel is downloaded
+            ss.ClearCache();
+            var model2 = await repo2.GetAllAsync();
+
+            //assert
+            Assert.IsTrue(repo.GetAllLazy().Contains(model));
+            Assert.IsTrue(model2.Count == 1);
+            Assert.IsTrue(model2[0].Content == "Hallo Welt 3");
+            
+            var history = await repo.GetHistoryAsync(model);
+            Assert.IsTrue(history.Count == 3);
+            Assert.IsTrue(history[0].Model.Content == "Hallo Welt!");
+            Assert.IsTrue(history[1].Model.Content == "Hallo Welt 2");
+            Assert.IsTrue(history[2].Model.Content == "Hallo Welt 3");
         }
     }
 }
