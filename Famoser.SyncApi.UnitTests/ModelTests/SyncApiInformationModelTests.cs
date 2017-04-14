87a0021ef8695b6bdc1a3251a313fbe623e53e32
famoser <git@famoser.ch>
2017-04-14 15:21:39 +0200

    adding more unit tests

---
+++ Famoser.SyncApi.UnitTests/ModelTests/SyncApiInformationModelTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Famoser.SyncApi.Enums;
+using Famoser.SyncApi.Helpers;
+using Famoser.SyncApi.Models;
+using Famoser.SyncApi.UnitTests.Helpers;
+using Famoser.SyncApi.UnitTests.Implementations;
+using Famoser.SyncApi.UnitTests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Famoser.SyncApi.UnitTests.ModelTests
+{
+    [TestClass]
+    public class SyncApiInformationModelTests
+    {
+        [TestMethod]
+        public void CheckDescription()
+        {
+            //arrange
+            var str = EnumHelper.EnumToString(SyncAction.CreateDevice);
+            var sam = new SyncActionInformation(SyncAction.CreateDevice);
+            Assert.IsTrue(sam.FullDescription.Contains("..."));
+            Assert.IsTrue(sam.FullDescription.Contains(str));
+
+            var errorStr = EnumHelper.EnumToString(SyncAction.CreateDevice);
+            sam.SetSyncActionResult(SyncActionError.LocalFileAccessFailed);
+            Assert.IsTrue(sam.FullDescription.Contains("failed"));
+            Assert.IsTrue(sam.FullDescription.Contains(str));
+            Assert.IsTrue(sam.FullDescription.Contains(errorStr));
+
+            sam.SetSyncActionResult(SyncActionError.None);
+            Assert.IsTrue(!sam.FullDescription.Contains("..."));
+            Assert.IsTrue(sam.FullDescription.Contains(str));
+        }
+    }
+}
--- Famoser.SyncApi.UnitTests/ServiceTests/ApiStorageTests.cs
+++ Famoser.SyncApi.UnitTests/ServiceTests/ApiStorageTests.cs
@@ -14,7 +14,7 @@ namespace Famoser.SyncApi.UnitTests.ServiceTests
     public class ApiStorageTests
     {
         [TestMethod]
-        public async Task TestCleanUpSuccessfullAsnyc()
+        public async Task TestRemoveUserAsync()
         {
             //arrange
             var ss = new StorageService();
@@ -38,6 +38,34 @@ namespace Famoser.SyncApi.UnitTests.ServiceTests
             Assert.IsTrue(saveRes);
 
             Assert.IsTrue(model2.Count == 0);
+        }
+
+        [TestMethod]
+        public async Task TestRemoveDeviceAsync()
+        {
+            //arrange
+            var ss = new StorageService();
+            var testHelper = new TestHelper { StorageService = ss };
+            var repo = testHelper.SyncApiHelper.ResolveRepository<NoteModel>();
+            var model = new NoteModel { Content = "Hallo Welt!" };
+
+            var testHelper2 = new TestHelper { StorageService = testHelper.StorageService };
+            var repo2 = testHelper2.SyncApiHelper.ResolveRepository<NoteModel>();
+
+            //act
+            var saveRes = await repo.SaveAsync(model);
+            //remove user & all other stuff
+            await testHelper.SyncApiHelper.ApiDeviceRepository.RemoveAsync();
+            //three files: user & roaming x2
+            Assert.IsTrue(ss.CountAllCachedFiles() + ss.CountAllRoamingFiles() == 3);
+
+            //try to retrieve again
+            var model2 = await repo2.GetAllAsync();
+
+            //assert
+            Assert.IsTrue(saveRes);
+
+            Assert.IsTrue(model2.Count == 1);
         }
     }
 }
